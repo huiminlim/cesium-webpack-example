804c56ee256337e49be4b0d993d82dbcdd1b86f9
huiminlim <e0036379@u.nus.edu>
2023-10-28 22:51:13 +0800

    set natural earth as base map

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
-import { Ion, Viewer, Cartesian3, Math } from "cesium";
+import {
+  Viewer,
+  buildModuleUrl,
+  ImageryLayer,
+  TileMapServiceImageryProvider,
+} from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css";
 
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer("cesiumContainer", {});
+var imageryViewModels = [];
 
-// // Add Cesium OSM Buildings, a global 3D buildings layer.
-// viewer.scene.primitives.add(createOsmBuildings());
+// // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+// const viewer = new Viewer("cesiumContainer", {
+//   // Hide the base layer picker
+//   baseLayerPicker: false,
+//   // baseLayer: new createTileMapServiceImageryProvider({
+//   //   url: buildModuleUrl("/Assets/Textures/NaturalEarthII"),
+//   // }),
+// });
 
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-  orientation: {
-    heading: Math.toRadians(0.0),
-    pitch: Math.toRadians(-15.0),
-  },
+const viewer = new Viewer("cesiumContainer", {
+  baseLayerPicker: false,
+  baseLayer: ImageryLayer.fromProviderAsync(
+    TileMapServiceImageryProvider.fromUrl(
+      buildModuleUrl("Assets/Textures/NaturalEarthII")
+    )
+  ),
 });
+
+// var terrainProvider = new Cesium.CesiumTerrainProvider({
+//   url: "http://localhost:8080/terrain",
+// });
+
+// viewer.scene.terrainProvider = terrainProvider;
+// viewer.scene.globe.terrainExaggeration = 1.0;
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+// var tileset = viewer.scene.primitives.add(
+//   new Cesium.Cesium3DTileset({
+//     url: "http://localhost:3000/optimized-tiles/tileset.json",
+//   })
+// );
+// tileset.cacheBytes = 536870912 * 5;
+// tileset.debugColorizeTiles = true;
+// tileset.loadSiblings = true;
+
+// // Fly the camera to San Francisco at the given longitude, latitude, and height.
+// viewer.camera.flyTo({
+//   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+//   orientation: {
+//     heading: Math.toRadians(0.0),
+//     pitch: Math.toRadians(-15.0),
+//   },
+// });
