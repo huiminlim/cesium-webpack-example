62074399627226b2d9be5a0b1f1c699b2cc4965b
huiminlim <e0036379@u.nus.edu>
2023-10-28 21:58:19 +0800

    Remove default cesium ion key

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import { Ion, Viewer, Cartesian3, Math } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css";
 
-// Your access token can be found at: https://cesium.com/ion/tokens.
-// This is the default access token
-Ion.defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk";
-
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Viewer("cesiumContainer", {});
 
